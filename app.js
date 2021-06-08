31fb4a98537ae9bea609ce12fdda7544b5d138f8
minah0102 <minah0102@gmail.com>
2021-06-07 21:30:09 -0400

    fixed menu tab and post

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import { renderAuthForm } from "./auth/auth.js";
-import { fetchtokenPosts, fetchPosts, renderPosts, fetchCreateNewPost, deletePost } from "./posts/post.js";
+import { fetchPosts, renderPosts, createNewPost, deletePost } from "./posts/post.js";
+
+let allPosts = $(".all-posts");
 
 window.auth_state = {
   currentPost: null,
@@ -17,23 +19,42 @@ function isLoggedIn() {
   return token;
 }
 
-
-function registeredUserData(token) {
-  return fetch(
-    "https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/users/me",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch(console.error);
+function renderPublicPage() {
+  $(".messages").remove();
+  $(".post_create").remove();
+  fetchPosts();
+  renderPosts();
 }
+
+function renderLogginedInPage() {
+  fetchPosts();
+  renderPosts();
+}
+
+function renderPage() {
+  if (isLoggedIn()) {
+    return renderLogginedInPage();
+  }
+  return renderPublicPage();
+}
+
+
+// function registeredUserData(token) {
+//   return fetch(
+//     "https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/users/me",
+//     {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }
+//   )
+//     .then((response) => response.json())
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch(console.error);
+// }
 
 async function fetchMe(token) {
   try {
@@ -74,62 +95,91 @@ if (isLoggedIn()) {
 
 
 
-function createMessages(token) {
-  fetch('https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/posts/5e8929ddd439160017553e06/messages', {
-  method: "POST",
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-  },
-  body: JSON.stringify({
-    message: {
-      content
-    }
-  })
-}).then(response => response.json())
-  .then(result => {
-    console.log(result);
-  })
-  .catch(console.error);
-}
-createMessages();
+// function createMessages(token) {
+//   fetch('https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/posts/5e8929ddd439160017553e06/messages', {
+//   method: "POST",
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': `Bearer ${token}`
+//   },
+//   body: JSON.stringify({
+//     message: {
+//       content
+//     }
+//   })
+// }).then(response => response.json())
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(console.error);
+// }
+// createMessages();
 
-$('.messages').click(function() {
-  let newMessages = $(`
-  <div>
-  <input type="text" class="title" placeholder="Title" />
-  <input type="text" class="message" placeholder="Message" />
-  </div>
-  `)
-  return $('#new_tab').append(newMessages)
-})
+let newMessages = $(`
+  <div class="new-messages">
+    <h3>Send New Message!</h3>
+      <input type="text" class="title" placeholder="Title" /><br>
+      <input type="text" class="message" placeholder="Message" />
+      <button>Send</button>
+  </div>`);
+newMessages.remove();
 
-$('.post_cards').click(function() {
-  return renderPosts()
+$('.messages').click(function () {
+  newPostTab.remove();
+  let messageAppears = $('#new_tab').append(newMessages);
+  return messageAppears.toggle();
 });
 
-$('.post_create').click(function() {
-  let newPostTab = $(`
-  <div>
+$('.post_cards').click(function () {
+  return fetchPosts();
+});
+
+let newPostTab = $(`
+  <div class="new-post">
     <h3>Create New Post!</h3>
-    <input type="text" class="title" placeholder="Title" />
-    <input type="text" class="description" placeholder="Description" />
-    <input type="text" class="price" placeholder="Price" />
-    <input type="text" class="location" placeholder="Location" />
-    <input type="text" class="will_deliver" placeholder="Will Deliver" />
+      <input type="text" class="new-post-title" placeholder="Title" />
+      <input type="text" class="new-post-description" placeholder="Description" /><br>
+      <input type="text" class="new-post-price" placeholder="Price" />
+      <input type="text" class="new-post-location" placeholder="Location" />
+      <input type="text" class="new-post-will_deliver" placeholder="Will Deliver" />
+      <button class="new-post-button">Post</button>
   </div>
-  `)
-  return $('#new_tab').append(newPostTab);
+  `);
+newPostTab.remove();
+
+$('.post_create').click(function () {
+  newMessages.remove();
+  let createNewPostAppears = $('#new_tab').append(newPostTab);
+  return createNewPostAppears.toggle();
+});
+
+let postButton = $(".new-post-button");
+postButton.click(function (event) {
+  event.preventDefault();
+  const title = $("new-post-title").val();
+  const description = $("new-post-description").val();
+  const price = $("new-post-price").val();
+  const location = $("new-post-location").val();
+  const delivery = $('input[name="willDeliver"]:checked').val();
+  const willDeliver = delivery == "true";
+  const postData = { title, description, price, willDeliver };
+
+  if (!title || !description || !price || !location) {
+    return alert("Please fill out all fields!");
+  }
+
+  createNewPost(postData);
+
+
 })
 
-function showAllPosts() {
-  return fetchPosts();
+
+
+function bootstrap() {
+  // fetchPosts();
+  // renderPosts();
+  renderPage();
 }
 
-showAllPosts();
-fetchtokenPosts();
-fetchPosts();
-renderPosts();
-fetchCreateNewPost();
-deletePost();
-registeredUserData();
+
+bootstrap();
--- posts/post.js
+++ posts/post.js
@@ -1,7 +1,7 @@
 let allPosts = $(".all-posts");
 let token = localStorage.getItem("token") || "";
 
-const BASE_URL = "https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/posts";
+const BASE_URL = "https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT";
 
 export async function fetchPosts() {
   try {
@@ -15,80 +15,88 @@ export async function fetchPosts() {
     const result = await response.json();
     const posts = result.data.posts;
     posts.forEach((post) => {
-      const currentPostElement = renderPost(post);
-      $(".post_cards").append(currentPostElement);
+      const currentPostElement = renderPosts(post);
+      allPosts.append(currentPostElement);
     })
   } catch (error) {
     console.error(error);
   }
 }
 
-
-export function renderPosts({
-  title,
-  author: { username },
-  location,
-  description,
-  willDeliver,
-  price,
-  isAuthor
-}) {
+export function renderPosts(post) {
+  const {
+    title,
+    author: { username },
+    location,
+    description,
+    willDeliver,
+    price,
+    isAuthor
+  } = post;
   return $(`
     <div class="post">
-      <h1 class="title">${title}</h1>
-        <div>${description}</div>
-        <div>Posted by: <span>${username}</span></div>
-        <div>Location: <span>${location}</span></div>
-        <div>Price: <span>${price}</span></div>
+      <h1 class="title">${title}</h1> <br>
+        <div>${description}</div> <br>
+        <div><b>Posted by: </b><span>${username}</span></div>
+        <div><b>Location: </b><span>${location}</span></div>
+        <div><b>Price: </b><span>${price}</span></div>
+        <div><b>Will Deliver? </b><span>${willDeliver ? "YES" : "NO"}</span></div>
         <div class="footer">
         ${isAuthor ?
       `<button id="delete">Delete</button>` : `<button id="send-message">Send Message</button>`
     }
     `).data("posts", post);
-  
-    //return $(".all-posts").append(postCard);
 }
 
 
-export function fetchCreateNewPost(token) {
-  fetch("https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      post: {
-        title,
-        description,
-        price,
-        willDeliver,
+export async function createNewPost(userData) {
+  const {
+    title,
+    description,
+    price,
+    willDeliver
+  } = userData;
+  try{
+    const response = await fetch(`${BASE_URL}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
       },
-    }),
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch(console.error);
+      body: JSON.stringify({
+        post: {
+          title,
+          description,
+          price,
+          willDeliver,
+        },
+      }),
+    });
+    const result = await response.json();
+    fetchPosts();
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+  
 }
 
-
-export function deletePost(token) {
-  fetch(
-    "https://strangers-things.herokuapp.com/api/2101-VPI-RM-WEB-PT/posts/5e8d1bd48829fb0017d2233b",
-    {
+export async function deletePost(data) {
+  const ID = data.postData._id;
+  try {
+    let response = await fetch(`${ BASE_URL }/posts/${ ID }`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
+        Authorization: "Bearer " + token,
       },
-    }
-  )
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch(console.error);
+    });
+    response = await response.json();
+    fetchPosts();
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 
